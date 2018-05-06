f4c8f838da873fac23841ab6392df85271a35a12
sured415 <wtghnh@gmail.com>
2018-05-06 13:46:39 +0900

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <linux/types.h>
+#include <linux/netfilter.h>        /* for NF_ACCEPT */
+#include <errno.h>
+#include <arpa/inet.h>
+#include <netinet/in_systm.h>
+#include <libnet/libnet-macros.h>
+#include <libnet/libnet-headers.h>
+
+#include <libnetfilter_queue/libnetfilter_queue.h>
+
+u_int16_t a;
+
+static u_int32_t print_pkt (struct nfq_data *tb)
+{
+	int id = 0;
+	struct nfqnl_msg_packet_hdr *ph;
+	int ret;
+	unsigned char *data;
+
+	ph = nfq_get_msg_packet_hdr(tb);
+    	if (ph) {
+        	id = ntohl(ph->packet_id);
+    	}
+
+	ret = nfq_get_payload(tb, &data);
+	if(ret >= 0) {
+		struct libnet_ipv4_hdr* ipH = (struct libnet_ipv4_hdr *) data;
+//		data += (ipH->ip_hl)*4;
+		data += sizeof(struct libnet_ipv4_hdr);
+		if(ipH->ip_p == 80){
+			struct libnet_tcp_hdr* tcpH = (struct libnet_tcp_hdr *) data;
+			if(ntohs(tcpH->th_sport == 80) || ntohs(tcpH->th_dport == 80)) {
+				a = NF_DROP;
+			}
+			else	a = NF_ACCEPT;
+		}
+		else	a = NF_ACCEPT;
+	}
+
+	return id;
+}
+
+static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
+          struct nfq_data *nfa, void *data)
+{
+    u_int32_t id = print_pkt(nfa);
+
+	if(a == NF_DROP) printf("******* TCP Port 80 black *******\n");
+    	else printf("entering callback\n");
+
+    	return nfq_set_verdict(qh, id, a, 0, NULL);
+
+}
+
+int main(int argc, char **argv)
+{
+    struct nfq_handle *h;
+    struct nfq_q_handle *qh;
+    struct nfnl_handle *nh;
+    int fd;
+    int rv;
+    char buf[4096] __attribute__ ((aligned));
+
+    printf("opening library handle\n");
+    h = nfq_open();
+    if (!h) {
+        printf("error during nfq_open()\n");
+        exit(1);
+    }
+
+    printf("unbinding existing nf_queue handler for AF_INET (if any)\n");
+    if (nfq_unbind_pf(h, AF_INET) < 0) {
+        printf("error during nfq_unbind_pf()\n");
+        exit(1);
+    }
+
+    printf("binding nfnetlink_queue as nf_queue handler for AF_INET\n");
+    if (nfq_bind_pf(h, AF_INET) < 0) {
+        printf("error during nfq_bind_pf()\n");
+        exit(1);
+    }
+
+    printf("binding this socket to queue '0'\n");
+
+    qh = nfq_create_queue(h,  0, &cb, NULL);					// Queue create
+    if (!qh) {
+        printf("error during nfq_create_queue()\n");
+        exit(1);
+    }
+
+    printf("setting copy_packet mode\n");
+    if (nfq_set_mode(qh, NFQNL_COPY_PACKET, 0xffff) < 0) {
+        printf("can't set packet_copy mode\n");
+        exit(1);
+    }
+
+    fd = nfq_fd(h);
+
+    for (;;) {
+        if ((rv = recv(fd, buf, sizeof(buf), 0)) >= 0) {
+            printf("pkt received\n");
+            nfq_handle_packet(h, buf, rv);
+            continue;
+        }
+        if (rv < 0 && errno == ENOBUFS) {
+            printf("losing packets!\n");
+            continue;
+        }
+        perror("recv failed");
+        break;
+    }
+
+    printf("unbinding from queue 0\n");
+    nfq_destroy_queue(qh);
+
+#ifdef INSANE
+    printf("unbinding from AF_INET\n");
+    nfq_unbind_pf(h, AF_INET);
+#endif
+
+    printf("closing library handle\n");
+    nfq_close(h);
+
+    exit(0);
+}
